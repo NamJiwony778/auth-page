833609fdfb0495c93c7d9f63161f4baa574ff08e
NamJiwony778 <71044421+NamJiwony778@users.noreply.github.com>
2021-02-10 16:27:12 -0500

    deployment

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', routes);
+app.use('/', cors(corsOptions));
 
 // app.get("/", (req, res) => {
 //     res.json({ message: "Welcome to application." });
