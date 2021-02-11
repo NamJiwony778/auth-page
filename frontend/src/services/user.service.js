0f39babab13e34b7e8466f77f4bbe09a55674a81
NamJiwony778 <71044421+NamJiwony778@users.noreply.github.com>
2021-02-10 20:39:18 -0500

    d

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from './auth-header';
 
-const API_URL = process.env.NODE_ENV + "/signup" || "http://localhost:3001/api/";
+const API_URL = process.env.NODE_ENV + "/" || "http://localhost:3001/api/";
 
 class UserService {
     getProfile() {
